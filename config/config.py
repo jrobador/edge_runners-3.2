dac412faf81c5595333c5ddc1f6c402bc9eb68ea
Joaquín Robador <joaquinrobador@gmail.com>
2024-10-18 15:48:06 -0300

    model decision changed
    From Meta-Llama-3.2-11B to Llama-3.2-90B

--- config/config.py
+++ config/config.py
@@ -16,5 +16,5 @@ class Config:
 
     # Available models
     AVAILABLE_MODELS = [
-        "meta-llama/Meta-Llama-3.2-11B-Vision-Instruct-Turbo",
+        "meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
     ]
